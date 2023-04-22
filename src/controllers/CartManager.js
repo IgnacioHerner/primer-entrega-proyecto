104f79c077b02494cb17996156caebd419a4a5d4
Ignacio Herner <ignah71@gmail.com>
2023-04-22 14:54:07 -0300

    Finalizando cartManager

--- src/controllers/CartManager.js
+++ src/controllers/CartManager.js
@@ -1,5 +1,8 @@
 import { promises as fs } from 'fs'
 import { nanoid } from 'nanoid'
+import ProductManager from './ProductManager.js'
+
+const productAll = new ProductManager
 
 class CartManager {
     constructor() {
@@ -14,14 +17,49 @@ class CartManager {
     writeCarts = async (carts) => {
         await fs.writeFile(this.path, JSON.stringify(carts))
     };
-
+    
+    exist = async (id) => {
+        let carts = await this.readCarts();
+        return carts.find(cart => cart.id === id)
+    }
     addCart = async () => {
         let cartsOld = await this.readCarts();
         let id = nanoid(4)
         let cartsConcat = [{id : id, products : []}, ...cartsOld]
         await this.writeCarts(cartsConcat)
         return "Added Product"
+    };
+    getCartById = async (id) => {  
+        let cartById = await this.exist(id)
+        if(!cartById) return "Cart Not Found"
+        return cartById
+    };
+    addProductInCart = async (cartId, productId) => {
+        let cartById = await this.exist(cartId)
+        if(!cartById) return "Cart Not Found"
+        let productById = await productAll.exist(productId)
+        if(!productById) return "Product Not Found"
+        
+        let cartsAll = await this.readCarts()
+        let cartFilter = cartsAll.filter((cart) => cart.id != cartId)
+
+        if(cartById.products.some((product) => product.id === productId)){
+            let moreProductInCart = cartById.products.find((product) => product.id === productId)
+            moreProductInCart.cantidad++;
+            let cartsConcat = [cartById, ...cartFilter]
+            await this.writeCarts(cartsConcat)
+            return "Product added to cart"
+        }
+
+        cartById.products.push({id:productById.id, cantidad: 1})
+
+        let cartsConcated = [cartById, ...cartFilter]
+
+        await this.writeCarts(cartsConcated)
+        return "Product added to cart"
     }
+
+
 }
 
 export default CartManager
