b9e8e139af05786f95aa214fe66c146205e59ac5
Ignacio Herner <ignah71@gmail.com>
2023-05-02 16:59:33 -0300

    Aplicando websockets

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,4 +1,3 @@
-
 const socketClient = io() // Se completa el handshake
 
 
--- src/router/views.routes.js
+++ src/router/views.routes.js
@@ -4,11 +4,27 @@ import ProductManager from '../controllers/ProductManager.js'
 const product = new ProductManager();
 const ViewsRouter = Router()
 
+
 ViewsRouter.get('/', async (req, res) => {
     let allProducts = await product.getProducts()
     res.render('realTimeProducts', {
         products : allProducts
     })
+})
+
+ViewsRouter.post('/', async (req, res) => {
+    const data = req.body;
+    try {
+        // Agregar el producto a la base de datos
+        await product.addProducts(data);
+        // Renderizar la vista de productos en tiempo real con el nuevo producto agregado
+        const products = await product.getProducts();
+        res.render('realTimeProducts', { products });
+    } catch (error) {
+        // Manejar errores si ocurren al agregar el producto
+        console.error(error);
+        res.status(500).send('Error al agregar el producto');
+    }
 })
 
 
